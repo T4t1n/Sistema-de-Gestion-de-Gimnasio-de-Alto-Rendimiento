f44910c993ca5c934065726511af73294338ea6b
T4t1n <jaimiskytn@gmail.com>
2025-08-20 14:53:11 -0600

    Segundo commit

--- FitGMSport/src/main/java/com/t4t1n/fitgmsport/FitGMSport.java
+++ FitGMSport/src/main/java/com/t4t1n/fitgmsport/FitGMSport.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.t4t1n.fitgmsport;
 
 /**
  *
- * @author cmjyt
+ * @author Jaimisky
  */
 public class FitGMSport {
 
---
+++ FitGMSport/src/main/java/com/t4t1n/fitgmsport/recursos/Atleta.java
@@ -0,0 +1,15 @@
+
+package com.t4t1n.fitgmsport.recursos;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jaimisky
+ */
+public class Atleta {
+    
+    LocalDate fechaRegistro =  
+    
+    
+}
