909bce50b8dc86e0a5e1099de3b3b2fcbcea33f1
T4t1n <jaimiskytn@gmail.com>
2025-08-20 14:35:59 -0600

    Primer commit

---
+++ FitGMSport/src/main/java/com/t4t1n/fitgmsport/FitGMSport.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.t4t1n.fitgmsport;
+
+/**
+ *
+ * @author cmjyt
+ */
+public class FitGMSport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
