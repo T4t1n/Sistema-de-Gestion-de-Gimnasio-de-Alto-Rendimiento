b22faf62c937886e9581e785d3d77d0682ed4cf6
T4t1n <jaimiskytn@gmail.com>
2025-08-28 20:43:25 -0600

    Actualizacion 28/08/25

--- FitGMSport/src/main/java/com/t4t1n/fitgmsport/test.java
+++ FitGMSport/src/main/java/com/t4t1n/fitgmsport/test.java
@@ -10,6 +10,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
@@ -20,7 +26,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class test {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
 //        String input = "Trabajo físico intensivo";
 //        String withoutAccent = StringUtils.stripAccents(input.toUpperCase().replaceAll("\\s", ""));
@@ -44,30 +50,87 @@ public class test {
         
         
         Scanner sc = new Scanner(System.in);
+//        
+//        System.out.println("Escribe algo importante");
+//        String text = sc.nextLine();
+//        
+//        try{
+//            BufferedWriter wf = new BufferedWriter(new FileWriter("notes.txt", true));
+//            wf.write("Notas de alguien que maneja sus problemas");
+//            wf.append(text);
+//            wf.close();
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
         
-        System.out.println("Escribe algo importante");
-        String text = sc.nextLine();
+//        try {
+//            File file = new File("notes.txt");
+//            BufferedReader rf = new BufferedReader(new FileReader(file));
+//            System.out.println(rf.readLine());
+//            System.out.println(file.getAbsolutePath());
+//            
+//            rf.close();
+//        }catch(Exception e) {
+//            System.out.println(e);
+//        }
+
+
+
+//        String tex = "Competitivo";                
+//        Categoria text2 = Categoria.valueOf(tex.toUpperCase());
+//        Entrenamiento en1 = new Entrenamiento("Trabajo intensio fisico", text2);
+//        Entrenamiento en2 = new Entrenamiento("Trabajo demencial", text2);
+//        List<Entrenamiento> Entrenamiento = List.of(en1,en2);
+//        List<String> list;
+//        String entre = en1.toString();
+//        list = List.of(en1.toString(), en2.toString());
+//        Path rutaArchivo = Paths.get("notes.txt");
+//        Files.write(rutaArchivo, list);
+//        String contenido = Files.readString(rutaArchivo);
+//        System.out.println(contenido);
         
-        try{
-            BufferedWriter wf = new BufferedWriter(new FileWriter("notes.txt", true));
-            wf.write("Notas de alguien que maneja sus problemas");
-            wf.append(text);
-            wf.close();
-        }catch (Exception e){
-            System.out.println(e);
-        }
+//        List<Entrenamiento> entrenamientos1;
+//        List<String> ListaEn;
+//        File file = new File("Hola.txt");
+//        int contador = 0;
+//        while(contador < 4) {
+//            String tex3 = "Competitivo";                
+//            Categoria text4 = Categoria.valueOf(tex3.toUpperCase());
+//            Entrenamiento en = new Entrenamiento("Trabajo intensio fisico", text4);
+//            entrenamientos1 = List.of(en);
+//            ListaEn = List.of(en.toString());
+//            Path rutaHola = Paths.get("Hola.txt");
+//            Files.write(rutaHola, ListaEn);
+//            String contenido1 = Files.readString(rutaHola);
+//            System.out.println(contenido1);
+//            contador++;
+//        }
         
-        try {
-            File file = new File("notes.txt");
-            BufferedReader rf = new BufferedReader(new FileReader(file));
-            System.out.println(rf.readLine());
+        String seguir;
+        String descEntrenamiento;
+        String categoriaEntre;
+        List<Entrenamiento> listaEntrenamientos;
+        List<String> listaEntrenamientosTexto = new ArrayList();
+        File fileEntrenamientos = new File("Entrenamientos.txt");
+        do {
+            System.out.println("Escribe la descripcion de el entrenamiento: ");
+            descEntrenamiento = sc.nextLine();
+            System.out.println("Escribe la categoria a la que pertenecera este entrenamiento");
+            categoriaEntre = sc.nextLine();
+            Categoria ce = Categoria.valueOf(categoriaEntre.toUpperCase());
+            Entrenamiento enGlobal = new Entrenamiento(descEntrenamiento, ce);
+            listaEntrenamientos = List.of(enGlobal);
+            listaEntrenamientosTexto.add(listaEntrenamientos.toString());
+            Path rutaEntrenamientos = Paths.get("Entrenaminetos.txt");
+            Files.write(rutaEntrenamientos, listaEntrenamientosTexto);
+            System.out.println("Desesa seguir?");
+            seguir = sc.nextLine();
             
-            rf.close();
-        }catch(Exception e) {
-            System.out.println(e);
-        }
-        
-        
+            
+        }while(seguir.equalsIgnoreCase("S"));
+        Path rutaEntrenamientos = Paths.get("Entrenaminetos.txt");
+        String contenidoEn = Files.readString(rutaEntrenamientos);
+        System.out.println(contenidoEn);
         
     }
     
